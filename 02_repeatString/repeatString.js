c94a90ddc03df1eca91b80911c2c135354eb8e8b
Eugene Kim <theeugenekim@gmail.com>
2022-09-13 01:22:37 -0700

    Pass all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
 const repeatString = function(string, num) {
     var repeated_string = '';
+    // Edge-case fallback
+    if (num < 0) {
+        return("ERROR")
+    } else if (string == '') {
+        return('')
+    }
+
+    // Function call
     for (let i = 0; i < num; i++) {
         repeated_string += string;
     }

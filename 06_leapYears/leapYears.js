3c2bbe36ffdf087555747d260a30e251e17676aa
Eugene Kim <theeugenekim@gmail.com>
2022-09-13 15:49:42 -0700

    Enhanced code

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -3,23 +3,7 @@ const leapYears = function(year) {
 // However, years divisible by 100 are not leap years (such as 1800 and 1900)
 // unless they are divisible by 400 (like 1600 and 2000, 
 // which were in fact leap years). (Yes, it's all pretty confusing)
-
-var divisibleBy4 = (year % 4 == 0)
-var divisibleBy100 = (year % 100 == 0)
-var divisibleBy400 = (year % 400 == 0)
-
-
-  if (divisibleBy4 == true) {
-      if (divisibleBy100 == true && divisibleBy400 != true) {
-        return false;
-      } else {
-        return true;
-      }
-  } else {
-      return false;
-  }
- 
- 
+    return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
 };
 
 // Do not edit below this line

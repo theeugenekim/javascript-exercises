80b31769406530bd9608c3465e3fd51ca950cdec
Eugene Kim <theeugenekim@gmail.com>
2022-09-13 14:13:59 -0700

    Pass all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    var range = []
+    range.push(arguments[0])
+    range.push(arguments[1])
+    range.sort()
 
+    console.log(range)
+    // 1 + 2 + 3 + 4 = 15
+
+    var startingNumber = range[0]
+    var endingNumber = range[1]
+
+    if (startingNumber < 0 || endingNumber < 0 || // Negative numbers
+        typeof startingNumber != 'number' || typeof endingNumber != 'number') { // Value type is not int
+        return('ERROR')
+    }
+
+    var sum = 0;
+
+    for (let i=startingNumber; i <= endingNumber; i++) {
+        sum += i;
+    }
+    console.log(sum)
+    return sum;
 };
 
 // Do not edit below this line

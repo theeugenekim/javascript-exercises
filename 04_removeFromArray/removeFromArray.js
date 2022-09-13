bef707cd409a0555719f1434e5b16b746a74bc24
Eugene Kim <theeugenekim@gmail.com>
2022-09-13 03:59:10 -0700

    Update variable names to be more clear

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,28 +1,27 @@
 function removeFromArray(array, remove) {
+    
     // Define main array A
-    var array_a = arguments[0];
+    var originalValues = arguments[0];
     // Define array B for remove values
-    var array_b = [];
-    var array_c = [];
+    var excludeValues = [];
+    var filteredArray = [];
 
+    //  Declare originalValues
     for (let i = 1; i < arguments.length; i++) {
-        array_b.push(arguments[i]);
+        excludeValues.push(arguments[i]);
     }
 
-    // For i in array_a:
-    for (let i = 0; i < array_a.length; i++) {
-
-      // splice array a if the value exists in array b
-      var exists = array_b.includes(array_a[i])
-
-      if (exists === false) {
-        array_c.push(array_a[i])
+    // Declare excludeValues
+    for (let i = 0; i < originalValues.length; i++) {
+        var exists = excludeValues.includes(originalValues[i])
+        // If Value doesn't exist in excludeValues, add to filteredArray  
+        if (exists === false) {
+            filteredArray.push(originalValues[i])
       } else {
         // pass
       }
     }
-    return array_c;
-
+    return filteredArray;
   };
 
 // Do not edit below this line

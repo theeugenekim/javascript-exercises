44931027f6a493ffe8b6e6f45ef16040c73bcc43
Eugene Kim <theeugenekim@gmail.com>
2022-09-13 01:40:36 -0700

    Pass all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    var reversed_string_list = string.split('');
+    var reversed_String = '';
 
+    for (let i = reversed_string_list.length-1; i >= 0; --i) {
+        reversed_String += reversed_string_list[i];
+    }
+    
+    return reversed_String;
 };
 
 // Do not edit below this line

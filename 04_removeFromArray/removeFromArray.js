db6c991764d3fa5ca6a18d11f929a241d7410207
Eugene Kim <theeugenekim@gmail.com>
2022-09-13 03:51:44 -0700

    Pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,29 @@
-const removeFromArray = function() {
+function removeFromArray(array, remove) {
+    // Define main array A
+    var array_a = arguments[0];
+    // Define array B for remove values
+    var array_b = [];
+    var array_c = [];
 
-};
+    for (let i = 1; i < arguments.length; i++) {
+        array_b.push(arguments[i]);
+    }
+
+    // For i in array_a:
+    for (let i = 0; i < array_a.length; i++) {
+
+      // splice array a if the value exists in array b
+      var exists = array_b.includes(array_a[i])
+
+      if (exists === false) {
+        array_c.push(array_a[i])
+      } else {
+        // pass
+      }
+    }
+    return array_c;
+
+  };
 
 // Do not edit below this line
 module.exports = removeFromArray;

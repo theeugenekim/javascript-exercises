b269244aa577eb74db84b5025d690c3dfbb2f025
Eugene Kim <theeugenekim@gmail.com>
2022-09-13 01:12:47 -0700

    Configre and pass test 1

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    var repeated_string = '';
+    for (let i = 0; i < num; i++) {
+        repeated_string += string;
+    }
+    return repeated_string;
 };
 
 // Do not edit below this line
